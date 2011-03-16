6d58aca987ef47d3ac30dd0011fe3b4f5d17d5b0
Reil <f.israel@mail.utexas.edu>
2011-03-16 16:51:33 -0500

    Added targetted player as the 'triggerer' for onTarget. Added mobdamage event.

--- src/com/reil/bukkit/MonsterTriggers/MTListener.java
+++ src/com/reil/bukkit/MonsterTriggers/MTListener.java
@@ -26,13 +26,77 @@ public class MTListener extends EntityListener {
 				reason == TargetReason.PIG_ZOMBIE_TARGET){
 			log.info("Entity changing targets.");
 			if (target instanceof Player){
+				Player targetPlayer =(Player) target;
 				String targeter = event.getEntity().getClass().getName();
 				targeter = targeter.substring(targeter.lastIndexOf("Craft") + "Craft".length());
-				String[] eventToReplace =  new String[0];
-				String []eventReplaceWith = new String[0];
+				String[] eventToReplace =  {"<<targeter>>"};
+				String []eventReplaceWith = {targeter};
 				rTriggers.triggerMessagesWithOption("targetsplayer", eventToReplace, eventReplaceWith);
 				rTriggers.triggerMessagesWithOption("targetsplayer|" + targeter,eventToReplace,eventReplaceWith);
+				rTriggers.triggerMessagesWithOption(targetPlayer, "targetsplayer", eventToReplace, eventReplaceWith);
+				rTriggers.triggerMessagesWithOption(targetPlayer, "targetsplayer|" + targeter,eventToReplace,eventReplaceWith);
 			}
 		}
+	}
+	public void onEntityDamage(EntityDamageEvent event ){
+		String damageCause; 
+		String triggerOption;
+		if (event.getEntity() instanceof Player) return;
+		String damaged = event.getEntity().getClass().getName();
+		damaged = damaged.substring(damaged.lastIndexOf("Craft") + "Craft".length());
+		switch (event.getCause()) {
+			case CONTACT:
+				triggerOption = "contact";
+				damageCause = "touching something";
+				break;
+			case ENTITY_ATTACK:
+				triggerOption = "entity_attack";
+				damageCause = "being hit";
+				break;
+			case SUFFOCATION:
+				triggerOption = "suffocation";
+				damageCause = "suffocation";
+				break;
+			case FALL:
+				triggerOption = "fall";
+				damageCause = "falling";
+				break;
+			case FIRE:
+				triggerOption = "fire";
+				damageCause = "fire";
+				break;
+			case FIRE_TICK:
+				triggerOption = "fire_tick";
+				damageCause = "burning";
+				break;
+			case LAVA:
+				triggerOption = "lava";
+				damageCause = "lava";
+				break;
+			case DROWNING:
+				triggerOption = "drowning";
+				damageCause = "drowning";
+				break;
+			case BLOCK_EXPLOSION:
+				triggerOption = "block_explosion";
+				damageCause = "explosion";
+				break;
+			case ENTITY_EXPLOSION:
+				triggerOption = "entity_explosion";
+				damageCause = "creeper";
+				break;
+			case CUSTOM:
+				triggerOption = "custom";
+				damageCause = "the unknown";
+				break;
+			default:
+				triggerOption = "something";
+				damageCause = "something";
+				break;
+		}
+		String [] replaceThese = {"<<damage-cause>>"};
+		String [] withThese = {damageCause};
+		rTriggers.triggerMessagesWithOption("ondamage|" + damaged + "|" + triggerOption,replaceThese,withThese);
+		rTriggers.triggerMessagesWithOption("ondamage|" + damaged,replaceThese,withThese);
 	}
 }
